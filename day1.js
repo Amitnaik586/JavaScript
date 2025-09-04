8524a56167704b2b9e162a50baba48647e96909d
Amit Naik <amitnaik6363@gmail.com>
2025-09-04 19:18:16 +0530

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,24 @@
+/* Naming a variable in Java Script */
+
+var myName="Amit";
+var my123=123;
+var MY_name="Amit";
+
+
+
+/* Simple value exchanging program in Java Script */
+
+
+var a="4";
+var b="3";
+
+console.log("a is "+a);
+console.log("b is "+b);
+var c=a;
+a=b;
+b=c;
+console.log("after exchange the value");
+console.log("a is "+a);
+console.log("b is " +b);
+
+
