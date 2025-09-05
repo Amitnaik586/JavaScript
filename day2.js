46d5638e91e42cac85e68fd449a4aeab361f65fb
Amit Naik <amitnaik6363@gmail.com>
2025-09-05 14:57:26 +0530

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,29 @@
+/* Conditional Statemants */
+
+/* if Statements */
+
+let a = 30;
+if (a % 2 == 0) {
+  console.log("even");
+} 
+else {
+  console.log("odd");
+}
+
+/* else if Statements */
+
+if (a==20){
+  console.log(" currect number");
+
+}
+else if (a==30){
+  console.log("try again");
+
+}
+else if(a==20){
+  console.log("try again")
+}
+
+else if(a==10){
+  console.log("try again");
+}
