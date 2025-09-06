97d13cd6910b9aa2a6bae633a09d63897c666405
Amit Naik <amitnaik6363@gmail.com>
2025-09-06 11:54:58 +0530

    Create day3.js

---
+++ day3.js
@@ -0,0 +1,17 @@
+/* LOOPS */
+
+/* for Loop */
+
+for (let count = 1; count < +5; count++) {
+  console.log("hello world");
+}
+
+/* While Loop */
+
+let i = 0;
+while (i < +5) {
+  console.log("hello world");
+  i++;
+}
+
+
