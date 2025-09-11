f2f958c3f0e81be98fab96154a491a184c3c8447
Amit Naik <amitnaik6363@gmail.com>
2025-09-11 11:03:31 +0530

    Create day5.js

---
+++ day5.js
@@ -0,0 +1,21 @@
+/* Functions in Java Script */
+
+function function_name(a, b) {
+  sum = a + b;
+  console.log(sum);
+}
+
+function_name(5, 6);
+function_name(676, 876);
+function_name(4545, 7688);
+
+/* Arrow Function*/
+
+const functionNAme=(a,b)=>{
+    sum=a+b;
+    console.log(sum)
+}
+
+functionNAme(364,673)
+functionNAme(456,8475)
+
